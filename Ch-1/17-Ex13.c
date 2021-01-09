8972ae25b7d2a61695b6fa33f05e3e276fea2d59
Ahmed El Ghattas <ahmed.ghattas@outlook.com>
2021-01-09 09:39:03 +0200

    Ch 1

--- Ch-1/17-Ex13.c
+++ Ch-1/17-Ex13.c
@@ -5,22 +5,39 @@ Write a program to print a histogram of the legnths of words in its input.
 #include <stdio.h>
 #define ON 1
 #define OFF 0
+#define SIZE 100
 
-main(){
-    int reader, state, letters, words;
+int main(){
+    int reader, state, letters, words, i;
     state = OFF;
     letters = 0;
     words = 0;
+
+    int stack[SIZE];
+
+    for (i=0; i<SIZE; ++i){
+        stack[i] = 0;
+    }
+
     while((reader = getchar())!=EOF){
         if (reader == ' ' || reader == '\n' || reader == '\t'){
             if(state == ON){
                 ++words;
+                stack[words] = letters;
                 state = OFF;
             }
         }else{
             state = ON;
             ++letters;
         }
+
     }
-    printf("The total Number of words is: %d\nThe total Number of letters is: %d\n", words, letters);
+    int histogram[words];
+    histogram[0] = 0;
+    printf("Word\tLetters\n");
+    for (i=1; i<=words; ++i){
+        histogram[i] = stack[i] - stack[i-1];
+        printf("%d\t%d\n", i, histogram[i]);
+    }
+    return 0;
 }
